eb7fecc46f5cf2aa03c62f0a030fa42e75165221
Rafael <rafaelvoss94@gmail.com>
2025-06-18 16:26:56 -0300

    updated the output.css file.

--- src/components/main_components_classes.js
+++ src/components/main_components_classes.js
@@ -1,4 +1,4 @@
-// document.querySelector('body').setAttribute('class', `bg-[url('/src/assets/main_bg.png')] bg-cover bg-blend-overlay bg-slate-500 flex flex-col h-screen w-screen font-semibold`);
+document.querySelector('body').setAttribute('class', `bg-[url('/src/assets/main_bg.png')] bg-cover bg-blend-overlay bg-slate-500 flex flex-col h-screen w-screen font-semibold`);
 document.querySelector('main').setAttribute('class', 'flex flex-col h-full w-full px-4 py-6 overflow-auto grow');
 // class="flex flex-col h-full w-full px-10 py-6 overflow-auto grow"
 // class="flex flex-col h-full w-full px-4 py-6 overflow-auto grow"
